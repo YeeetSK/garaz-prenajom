b61abb61c9e332052d285aa8cdf4f8d1fa9e17b8
Yeet <113854826+YeeetSK@users.noreply.github.com>
2026-08-25 23:30:06 +0200

    Fix displayed garage dimensions

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const GARAGE_LISTING = {
   longDescription:
     "Garáž je vhodná pre osobné auto alebo ako skladový priestor. Má pohodlný vjazd, pevnú podlahu a uzamykateľnú bránu. Garáž je taktiež novo vymalovaná, a má elektrinu.",
   location: "Ružinov, Ďatelinová 5530/10",
-  dimensions: "5,50 × 2,80 m",
+  dimensions: "6,2 × 2,9 m",
   area: "18 m²",
   length: "6,2 m",
   width: "2,9 m",
